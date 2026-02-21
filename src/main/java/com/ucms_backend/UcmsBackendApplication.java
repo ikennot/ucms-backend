a0ff99d35d04d9348512f690d15bf29919de4d4b
ikennot <domdomkenneth23@gmail.com>
2026-02-22 01:21:49 +0800

    Initial commit

---
+++ src/main/java/com/ucms_backend/UcmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ucms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucms_backend/UcmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
