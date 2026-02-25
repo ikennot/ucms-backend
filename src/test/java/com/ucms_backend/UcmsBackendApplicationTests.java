bf53f66b769a1db9db4a6ba7d74abcefe4916e49
XpnsiveSharks <aban.marynelle.tesoro@gmail.com>
2026-02-26 02:29:21 +0800

    fix(test): exclude DataSource auto-config to fix ci context load failure

--- src/test/java/com/ucms_backend/UcmsBackendApplicationTests.java
+++ src/test/java/com/ucms_backend/UcmsBackendApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ucms_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 class UcmsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
